1c09dc0078a359d450a18dd0f63a8df6280c195d
ramilmammadov <ramil.mammadov.95@gmail.com>
2020-06-29 02:32:02 +0400

    .

--- jenkins-agents/k8-custom-build-agent/docker-build.sh
+++ jenkins-agents/k8-custom-build-agent/docker-build.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 set -x
-docker build . -t kubertest-automated1
-docker tag kubertest-automated1 gcr.io/winter-monolith-281508/kubertest-automated:1.0
+docker build . -t kubertest-automated:1.0
+docker tag kubertest-automated:1.0 gcr.io/winter-monolith-281508/kubertest-automated:1.0
 docker push  gcr.io/winter-monolith-281508/kubertest-automated:1.0
--- src/main/java/com/example/demo/controller/HomeController.java
+++ src/main/java/com/example/demo/controller/HomeController.java
@@ -8,6 +8,6 @@ public class HomeController {
 
     @GetMapping("/")
     public String ok() {
-        return "-----------------------------------";
+        return "success";
     }
 }
