740c8e3d07898788b838fccc2b31d0480e8fd89f
ramilmammadov <ramil.mammadov.95@gmail.com>
2020-06-29 01:27:44 +0400

    .

--- Jenkinsfile
+++ Jenkinsfile
@@ -1,10 +1,10 @@
 pipeline{
     agent {
         kubernetes{
-            label 'gcloud-k8s-gradle-gcloud-helm'
+            label 'java-k8s-gradle-gcloud-helm'
             idleMinutes 5
             yamlFile 'jenkins-agents/k8-build-agent/jenkins-agent.yaml'
-            defaultContainer 'gcloud-k8s-gradle-gcloud-helm'
+            defaultContainer 'java-k8s-gradle-gcloud-helm'
         }
     }
     stages{
--- jenkins-agents/k8-build-agent/docker-build.sh
+++ jenkins-agents/k8-build-agent/docker-build.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 set -x
-docker build . -t gcloud-k8s-gradle-gcloud-helm:1.0
-docker tag gcloud-k8s-gradle-gcloud-helm:1.0 gcr.io/winter-monolith-281508/gcloud-k8s-gradle-gcloud-helm:1.0
-docker push  gcr.io/winter-monolith-281508/gcloud-k8s-gradle-gcloud-helm:1.0
+docker build . -t java-k8s-gradle-gcloud-helm:1.0
+docker tag java-k8s-gradle-gcloud-helm:1.0 gcr.io/winter-monolith-281508/java-k8s-gradle-gcloud-helm:1.0
+docker push  gcr.io/winter-monolith-281508/java-k8s-gradle-gcloud-helm:1.0
