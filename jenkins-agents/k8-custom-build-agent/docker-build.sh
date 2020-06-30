0ce8e406920596be70b415ad025998248d89793c
ramilmammadov <ramil.mammadov.95@gmail.com>
2020-06-30 23:38:29 +0400

    .

--- jenkins-agents/k8-custom-build-agent/docker-build.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-set -x
-docker build . -t kubertest-automated:10.0
-docker tag kubertest-automated:10.0 gcr.io/winter-monolith-281508/kubertest-automated:10.0
-docker push  gcr.io/winter-monolith-281508/kubertest-automated:10.0
